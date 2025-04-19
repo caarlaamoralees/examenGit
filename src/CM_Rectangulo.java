e663d2aece95c834dbf5a0bed7cb267dbc06ed1f
carlamorales <carlamorales@localhost>
2025-04-19 11:08:11 +0200

    CM_commit de la segunda clase rectangulo

---
+++ src/CM_Rectangulo.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author carlamorales
+ */
+public class CM_Rectangulo {
+    
+    int id;
+    double ladoA;
+    double ladoB;
+    String Color;
+
+    public CM_Rectangulo() {
+    }
+
+    public CM_Rectangulo(int id, double ladoA, double ladoB, String Color) {
+        this.id = id;
+        this.ladoA = ladoA;
+        this.ladoB = ladoB;
+        this.Color = Color;
+    }
+
+    // getters
+    public int getId() {
+        return id;
+    }
+
+    public double getLadoA() {
+        return ladoA;
+    }
+
+    public double getLadoB() {
+        return ladoB;
+    }
+
+    public String getColor() {
+        return Color;
+    }
+
+    //setters
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setLadoA(double ladoA) {
+        this.ladoA = ladoA;
+    }
+
+    public void setLadoB(double ladoB) {
+        this.ladoB = ladoB;
+    }
+
+    public void setColor(String Color) {
+        this.Color = Color;
+    }
+    
+}
